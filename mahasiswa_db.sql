f98b1ed30dd3e44092cc189c5b04b828c6cf7961
Wyatt Matthew <wyatt.honny06@gmail.com>
2025-09-17 21:48:18 +0800

    Add database
    MySQL + PhpMyAdmin

--- mahasiswa_db.sql
+++ mahasiswa_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 15, 2025 at 07:24 AM
+-- Generation Time: Sep 17, 2025 at 03:47 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
